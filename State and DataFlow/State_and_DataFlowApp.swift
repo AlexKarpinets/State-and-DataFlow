e5dfef0d7391832a2c4cf07b24e88ce22310c4eb
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-09-14 21:30:34 +0300

    Initial Commit

---
+++ State and DataFlow/State_and_DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_and_DataFlowApp.swift
+//  State and DataFlow
+//
+//  Created by Karpinets Alexander on 14.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct State_and_DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
